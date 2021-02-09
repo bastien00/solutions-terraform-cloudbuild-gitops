493ccaeb641053607d12ecbae9b683b498082f16
bcsl6337 <bastien.beaurin@orange.com>
2021-02-09 16:20:52 +0100

    correction2

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -25,23 +25,6 @@ provider "google" {
 }
 
 
-module "vpc" {
-  source  = "../../modules/vpc"
-  project = "${var.project}"
-  env     = "${local.env}"
-}
-
-module "http_server" {
-  source  = "../../modules/http_server"
-  project = "${var.project}"
-  subnet  = "${module.vpc.subnet}"
-}
-
-module "firewall" {
-  source  = "../../modules/firewall"
-  project = "${var.project}"
-  subnet  = "${module.vpc.subnet}"
-}
 
 resource "google_project_service" "service" {
 
