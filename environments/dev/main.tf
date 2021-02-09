a8ca44c83326e4693ca58d39d8a9346c2ef1f49c
bcsl6337 <bastien.beaurin@orange.com>
2021-02-09 17:49:45 +0100

    correction6

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -42,7 +42,7 @@ resource "google_bigquery_dataset" "dataset" {
   dataset_id                  = "example_dataset"
   friendly_name               = "test"
   description                 = "This is a test description"
-  location                    = "EU"
+  location                    = "europe-west3"
   default_table_expiration_ms = 3600000
 
   labels = {
